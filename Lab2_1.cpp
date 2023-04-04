5a4ee61aa29ea67e402175a4e226b346057e3eac
KirillllKabsabarov <127861873+KirillllKabsabarov@users.noreply.github.com>
2023-04-04 19:36:24 +0500

    Add files via upload

---
+++ Lab2_1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <conio.h>
+
+class Square {
+public:
+	int width;
+	int height;
+	std::string name;
+	Square() {//конст. по умол-ию
+		std::cout << "Square()\n";
+		width = 0;
+		height = 0;
+		name = "Square_1";
+	}
+	Square(int width, int height) {//конст. с парам-ми
+		std::cout << "Square(int width, int height)\n";
+		this->width = width;
+		this->height = height;
+		name = "Square_2";
+	}
+	Square(const Square& p) {//копи конст.
+		std::cout << "Square(const Square &p)\n";
+		width = p.width;
+		height = p.height;
+		name = "Square_3";
+	}
+	~Square() {//деструктор 
+		std::cout << width << ", " << height << ", " << name << "\n";
+		std::cout << "~Square()\n";
+	}
+};
+
+int main() {
+	{
+		Square p;
+		Square p2(20, 20);
+		Square p3(p2);
+	}
+
+	_getch();
+	return 0;
+}

b9ecfc662fc8ae123691322d41f4566aa6c11be7
KirillllKabsabarov <127861873+KirillllKabsabarov@users.noreply.github.com>
2023-04-04 19:37:37 +0500

    Add files via upload

---
+++ Lab2_2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <conio.h>
+
+class Square {
+public:
+	int width;
+	int height;
+	std::string name;
+	Square() {//конст. по умол-ию
+		std::cout << "Square()\n";
+		width = 0;
+		height = 0;
+		name = "Square_1";
+	}
+	Square(int width, int height) {//конст. с парам-ми
+		std::cout << "Square(int width, int height)\n";
+		this->width = width;
+		this->height = height;
+		name = "Square_2";
+	}
+	Square(const Square& p) {//копи конст.
+		std::cout << "Square(const Square &p)\n";
+		width = p.width;
+		height = p.height;
+		name = "Square_3";
+	}
+	~Square() {//деструктор 
+		std::cout << width << ", " << height << ", " << name << "\n";
+		std::cout << "~Square()\n";
+	}
+};
+
+int main() {
+	
+	Square *p = new Square;
+	Square *p2 = new Square(20, 20);
+	Square *p3 = new Square(*p2);
+	//удаление динамических объектов  
+	delete p;
+	delete p2;
+	delete p3;
+
+	_getch();
+	return 0;
+}
